a6137020b9995c55b9aeabc6287690a3841f6211
David Deville <david.deville59@gmail.com>
2022-11-24 17:35:26 +0100

    resolve giga problem

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -15,6 +15,3 @@ application.register("hello", HelloController)
 
 import MapController from "./map_controller"
 application.register("map", MapController)
-
-import StimulusFlatpickrController from "./stimulus_flatpickr_controller"
-application.register("stimulus-flatpickr", StimulusFlatpickrController)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_11_24_143418) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,7 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_143418) do
     t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "booking_id"
+    t.bigint "booking_id", null: false
     t.index ["booking_id"], name: "index_reviews_on_booking_id"
   end
 
